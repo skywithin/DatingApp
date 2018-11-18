88c4c31830d12a4f75cdc41b6e37efb0edeb8626
Max <sydneymax@gmail.com>
2018-11-18 22:20:47 +1100

    016 - Adding CORS support to the API

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -34,6 +34,8 @@ namespace DatingApp.API
                         Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,6 +45,8 @@ namespace DatingApp.API
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
             app.UseMvc();
         }
